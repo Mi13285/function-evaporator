26f0391cef0c5ed9c40b8525773840958f45625b
Mi13285 <mixkud95@inbox.ru>
2024-12-30 15:46:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function evaporator(content, evap_per_day, threshold) {
+  let result = 0;
+  let percentage = 100;
+  while (percentage > threshold) {
+    percentage = percentage - percentage * (evap_per_day / 100);
+    result += 1;
+  }
+  return result;
+}
+console.log(evaporator(10, 10, 10));
